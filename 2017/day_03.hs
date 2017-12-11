2e3134448fb7e8f179d9650a0c30e201920d0b44
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2017-12-12 08:46:31 +1300

    2017 Day 03 Part 1

---
+++ 2017/day_03.hs
@@ -0,0 +1,13 @@
+module Main where
+
+square :: Int -> [Int]
+square 0 = [1]
+square n =
+  let prev_square = square (n - 1) in
+    let a = last prev_square + 1 in
+      [a..a + (8 * 2 ^ (n - 1)) - 1]
+
+--
+
+main :: IO ()
+main = putStrLn $ show $ square 3
