6312120b02ebb319d07e821314f4b76b55d4e1a3
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2017-12-20 12:29:11 +1300

    Enable testing with Tasty.HUnit

--- 2017/haskell/test/Spec.hs
+++ 2017/haskell/test/Spec.hs
@@ -1,2 +1,14 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Day01
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain (testGroup "My tests" [year2017day01part1])
+
+year2017day01part1 :: TestTree
+year2017day01part1 = testCase "2017 Day 01 part 1"
+  (assertEqual "" (sumCaptcha "3") 3)
