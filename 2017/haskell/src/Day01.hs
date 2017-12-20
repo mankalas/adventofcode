ba3e8d15ddc7ed9e2c438bed196a861b7cef1c86
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2017-12-20 16:23:01 +1300

    Improve Day01, inspired from Nicu.
    Slight difference with Nicu on part2: I simply drop half the list
    before zipping, because the length of 'zip' output is the same as the
    shortest input.

--- 2017/haskell/src/Day01.hs
+++ 2017/haskell/src/Day01.hs
@@ -1,39 +1,13 @@
-module Day01 where
+module Day01 (part1, part2) where
 
 import Data.Char
-
-sumCaptcha :: String -> Int
-sumCaptcha [] = 0
-sumCaptcha [x] = digitToInt x
-sumCaptcha (x1:x2:xs) =
-  let increment = if x1 == x2 then digitToInt x1 else 0 in
-    increment + sumCaptcha (x2:xs)
-
-validDigit2 :: String -> Bool
-validDigit2 string =
-  let half_length = (length string) `div` 2
-      other_char = string !! half_length in
-      head string == other_char
-
-rotate :: String -> String
-rotate [] = []
-rotate (x:xs) = xs ++ [x]
-
-sumCaptchaRotate :: Int -> String -> Int
-sumCaptchaRotate 0 _ = 0
-sumCaptchaRotate n string =
-  let increment = if validDigit2 string
-                  then digitToInt $ head string
-                  else 0 in
-    increment + sumCaptchaRotate (n - 1) (rotate string)
-
-sumCaptcha2 :: String -> Int
-sumCaptcha2 string = sumCaptchaRotate (length string) string
 
 -- exports
 
 part1 :: String -> String
-part1 input = show $ sumCaptcha input
+part1 l1@(x:xs) = show $ sum [ digitToInt a | (a,b) <- zip l1 l2, a == b ]
+  where l2 = xs ++ [x]
 
 part2 :: String -> String
-part2 input = show $ sumCaptcha2 input
+part2 l1 = show $ sum [ 2 * digitToInt a | (a,b) <- zip l1 l2, a == b ]
+  where l2 = drop (length l1 `div` 2) l1
