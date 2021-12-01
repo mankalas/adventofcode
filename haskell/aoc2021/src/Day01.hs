6390322c183b6a52a0e51f35873983bd8ed89d91
Vincent Boucheny <mankalas@gmail.com>
2021-12-02 10:01:21 +1300

    [Haskell][Day01] Simpler implementation

--- haskell/aoc2021/src/Day01.hs
+++ haskell/aoc2021/src/Day01.hs
@@ -7,29 +7,28 @@ import AoC
 import IntHelper
 import MyList
 
+type Window = (Int, Int, Int)
+
 -- Compare a pair of consecutive depths
 depthIncrease :: (Int, Int) -> Bool
 depthIncrease (a, b) = a < b
 
 -- Compare a pair of depth windows by comparing their sum
-depthWindowIncrease :: ((Int, Int, Int), (Int, Int, Int)) -> Bool
-depthWindowIncrease (a, b) = (sum3 a) < (sum3 b)
+windowIncrease :: (Window, Window) -> Bool
+windowIncrease (a, b) = (sum3 a) < (sum3 b)
   where
     sum3 (a, b, c) = a + b + c
 
--- Zip the original list with itself rotateted once and rotateted
--- twice, generating the sliding windows. We then compare the
--- consecutive windows.
-depthWindowsIncreases :: [Int] -> Int
-depthWindowsIncreases d1 =
+createWindows :: [Int] -> [Window]
+createWindows d1 =
   let d2 = rotate d1
       d3 = rotate d2
-      d = zip3 d1 d2 d3
-   in length $ filterConsecutive depthWindowIncrease d
+   in zip3 d1 d2 d3
 
 -- exports
 part1 :: Solution
 part1 = show . length . filterConsecutive depthIncrease . linesToInts
 
 part2 :: Solution
-part2 = show . depthWindowsIncreases . linesToInts
+part2 =
+  show . length . filterConsecutive windowIncrease . createWindows . linesToInts
