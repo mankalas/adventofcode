fd03a20fb5b3ec12df2007e128ef91f0448c5856
Vincent Boucheny <mankalas@gmail.com>
2021-12-08 10:25:28 +1300

    [Haskell][2021] Day 04 skeleton

---
+++ haskell/aoc2021/src/Day04.hs
@@ -0,0 +1,14 @@
+module Day04 (parts) where
+
+import AoC
+
+-- exports
+
+part1 :: PartSolution
+part1 = id
+
+part2 :: PartSolution
+part2 = id
+
+parts :: DaySolutions
+parts = (4, part1, part2)
---
+++ haskell/aoc2021/test/Day04Spec.hs
@@ -0,0 +1,14 @@
+-- |
+module Main where
+
+import AoC (testDay)
+import Test.Tasty
+
+import Day04
+
+tests = [testDay 2021 Day04.parts]
+
+-- main
+main :: IO ()
+main = do
+  defaultMain (testGroup "AoE 2021" tests)
