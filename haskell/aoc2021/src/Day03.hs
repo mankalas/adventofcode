5bdd8f2947a4d23e1c7e6714d5f5f2cc39f6efb0
Vincent Boucheny <mankalas@gmail.com>
2021-12-07 13:23:32 +1300

    [Haskell][2021] Day 03 part 2

--- haskell/aoc2021/src/Day03.hs
+++ haskell/aoc2021/src/Day03.hs
@@ -3,36 +3,55 @@ module Day03
   ) where
 
 import Data.List
+import Data.Ord (comparing)
+import MyTuple (ap2, call2, dot2, map2, toList)
 
 import AoC
 import StringHelper
 
-gammaCriteria :: String -> Char
-gammaCriteria l =
-  let (zeros, ones) = partition ((==) '0') l
-   in if (length zeros) > (length ones)
-        then '0'
-        else '1'
+-- Split the list between 0s and 1s, compare the length of the
+-- resulting sub-lists and return the head (can be any char really) of
+-- the sub-list which satisfies the given "election" function.
+commonality ::
+     ((String -> String -> Ordering) -> [String] -> String) -> String -> Char
+commonality electBy =
+  head . electBy (comparing length) . toList . partition ((==) '0')
 
-espilonCriteria :: String -> Char
-espilonCriteria l =
-  let (zeros, ones) = partition ((==) '0') l
-   in if (length zeros) > (length ones)
-        then '1'
-        else '0'
+mostCommon :: String -> Char
+mostCommon = commonality maximumBy
+
+leastCommon :: String -> Char
+leastCommon = commonality minimumBy
+
+-- Get the relevant bit of the i-th column of the input, keep the
+-- values whose i-th bit equals the relevant one, and keep going with
+-- the next column, until there's only one vlaue left.
+filter' :: (String -> Char) -> [String] -> Int -> String
+filter' _ [x] _ = x
+filter' fBit values i =
+  let relevantBit = fBit $ (transpose values !! i)
+      survivors = filter (\e -> (e !! i) == relevantBit) values
+   in filter' fBit survivors (i + 1)
+
+oxygenCriteria :: [String] -> String
+oxygenCriteria values = filter' mostCommon values 0
+
+co2Criteria :: [String] -> String
+co2Criteria values = filter' leastCommon values 0
 
 -- exports
 part1 :: PartSolution
 part1 input =
-  let nthBits = transpose $ lines input
-      gamma = rating gammaCriteria nthBits
-      epsilon = rating espilonCriteria nthBits
-   in show $ gamma * epsilon
-  where
-    rating f = binaryToInt . map f
+  let (g, e) =
+        ap2 binaryToInt $
+        map2 (mostCommon, leastCommon) $ transpose $ lines input
+   in show $ g * e
 
 part2 :: PartSolution
-part2 = id
+part2 input =
+  let (o, c) =
+        call2 (dot2 binaryToInt (oxygenCriteria, co2Criteria)) $ lines input
+   in show $ o * c
 
 parts :: DaySolutions
 parts = (3, part1, part2)
---
+++ haskell/common/src/MyTuple.hs
@@ -0,0 +1,17 @@
+-- |
+module MyTuple where
+
+toList :: (a, a) -> [a]
+toList (a, b) = [a, b]
+
+ap2 :: (a -> b) -> (a, a) -> (b, b)
+ap2 f (a, b) = (f a, f b)
+
+call2 :: (a -> b, a -> c) -> a -> (b, c)
+call2 (f, g) a = (f a, g a)
+
+map2 :: (a -> b, a -> c) -> [a] -> ([b], [c])
+map2 (f, g) a = (map f a, map g a)
+
+dot2 :: (b -> c) -> (a1 -> b, a2 -> b) -> (a1 -> c, a2 -> c)
+dot2 h (f, g) = (h . f, h . g)
