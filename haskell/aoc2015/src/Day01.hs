3aec541b5e488c59104a5773e8a0bcae289f54b3
Vincent Boucheny <mankalas@gmail.com>
2021-12-22 11:44:35 +1300

    [Haskell] 2015 Day 01 part 1

--- haskell/aoc2015/src/Day01.hs
+++ haskell/aoc2015/src/Day01.hs
@@ -1,14 +1,20 @@
-module Day01 (parts) where
+module Day01
+  ( parts
+  ) where
 
 import AoC
 
--- exports
+step :: Char -> Int -> Int
+step '(' i = i + 1
+step ')' i = i - 1
+step _ i = i
 
+-- exports
 part1 :: PartSolution
-part1 = id
+part1 s = show $ foldr step 0 s
 
 part2 :: PartSolution
-part2 = id
+part2 s = ""
 
 parts :: DaySolutions
 parts = (1, part1, part2)
