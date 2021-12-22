a64275ea55cf24ef7a1a00ac1e7872ed9f8b451d
Vincent Boucheny <mankalas@gmail.com>
2021-12-22 13:05:52 +1300

    [Haskell][2015][01][2]

--- haskell/aoc2015/src/Day01.hs
+++ haskell/aoc2015/src/Day01.hs
@@ -9,12 +9,19 @@ step '(' i = i + 1
 step ')' i = i - 1
 step _ i = i
 
+basement :: String -> Int -> Int -> Int
+basement [] _ idx = idx
+basement (x:xs) pos idx =
+  if (pos < 0)
+    then idx
+    else basement xs (step x pos) (idx + 1)
+
 -- exports
 part1 :: PartSolution
 part1 s = show $ foldr step 0 s
 
 part2 :: PartSolution
-part2 s = ""
+part2 s = show $ basement s 0 0
 
 parts :: DaySolutions
 parts = (1, part1, part2)
